5faa9fed1c7d3b2579a3f551afcf37a83404d367
E1_Jyoti_83865 <jsbhosale.com>
2024-03-24 11:37:32 +0530

    Assignment-4

---
+++ Assignment 4/Q15.c
@@ -0,0 +1,42 @@
+//to print a given number in binary format.
+
+#include <stdio.h>
+
+void printBinary(int num) 
+{
+    if (num == 0) 
+	{
+        printf("0");
+        return;
+    }
+
+    int binary[32];
+    int i = 0;
+
+    while (num > 0)
+	{
+        binary[i] = num % 2;
+        num = num / 2;
+        i++;
+    }
+
+    for (int j = i - 1; j >= 0; j--) 
+	{
+        printf("%d", binary[j]);
+    }
+}
+
+int main() 
+{
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    printf("Binary representation: ");
+    printBinary(number);
+    
+	printf("\n");
+
+    return 0;
+}
+
